fix plotting with no units, add data to rdi parser output

python/plotNetCDF.py
@@ -185,8 +185,11 @@ for path_file in sys.argv[1:len(sys.argv)]:
         qc_m = np.ma.masked_where((qc <= 3) | (qc == 8), var)
         plt.plot(dt_time, qc_m, 'ro')
 
-        if plot_var.units == 'dbar':
-            plt.gca().invert_yaxis()
+        try:
+            if plot_var.units == 'dbar':
+                plt.gca().invert_yaxis()
+        except AttributeError:
+            pass
 
         #fig.autofmt_xdate()
         plt.grid()
@@ -204,7 +207,10 @@ for path_file in sys.argv[1:len(sys.argv)]:
 
         # add units to Y axis
 
-        plt.ylabel(plot + ' (' + plot_var.units + ')')
+        try:
+            plt.ylabel(plot + ' (' + plot_var.units + ')')
+        except AttributeError:
+            pass
 
         date_time_start = None
         # plot only the time of deployment

python/rdi2netCDF.py
@@ -17,7 +17,6 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import sys
-import re
 
 import datetime
 from netCDF4 import num2date, date2num
@@ -27,11 +26,11 @@ import struct
 
 header_decoder = {'keys': ['spare', 'dataTypes'],
                   'unpack': "<BB"}
-fixed_decoder = {'keys': ['cpuVER', 'cpuREV', 'sysConfig', 'read', 'lag_len', 'no_beam', 'num_cells',
+fixed_decoder = {'keys': ['cpuVER', 'cpuREV', 'sysConfig', 'read', 'lag_len', 'num_beam', 'num_cells',
                           'pings_per_ensemble', 'cell_length', 'blank_after_tx', 'profile_mode', 'low_corr',
                           'code_reps', 'gd_min', 'error_vel_max', 'tpp_min', 'tpp_sec', 'tpp_hun_sec', 'coord_trans',
-                          'head_alignment', 'head_bias', 'sensor_source', 'sensor_avaliable', 'bin1_dist',
-                          'xmit_pulse_len', 'ref_layer', 'false_target_thredh', 'spare', 'tx_lag_dist', 'cpu_board',
+                          'head_alignment', 'head_bias', 'sensor_source', 'sensor_available', 'bin1_dist',
+                          'xmit_pulse_len', 'ref_layer', 'false_target_thresh', 'spare', 'tx_lag_dist', 'cpu_board',
                           'system_bandwidth', 'system_power', 'spare2', 'inst_serial', 'beam_angle'],
                  'unpack': "<BBHBBBBHHHBBBBHBBBBHHBBHHHBBHQHBBLB"}
 variable_decoder = {'keys': ['ensemble_no', 'year', 'month', 'day', 'hour', 'minute', 'second', 'hsec',
@@ -40,14 +39,41 @@ variable_decoder = {'keys': ['ensemble_no', 'year', 'month', 'day', 'hour', 'min
                              'pitch_stdev', 'roll_stdev', 'adc0', 'adc1', 'adc2', 'adc3', 'adc4', 'adc5', 'adc6', 'adc7',
                              'error_status', 'spare1', 'pressure', 'press_variance', 'spare2', 'rtc_cen',
                              'rtc_year', 'rtc_month', 'rtc_day', 'rtc_hour', 'rtc_min', 'rtc_sec', 'rtc_hsec'],
-                    'unpack': "<H7BB8HBBBBBB8BIHIIB8B"}
+                    'unpack': "<H7BB4H4hBBBBBB8BIHiiB8B"}
+
+inst_system_decoder = {}
+inst_system_decoder[0] = '75kHz'
+inst_system_decoder[1] = '150kHz'
+inst_system_decoder[2] = '300kHz'
+inst_system_decoder[3] = '600kHz'
+inst_system_decoder[4] = '1200kHz'
+inst_system_decoder[5] = '2400kHz'
+
+inst_coords_decoder = {}
+inst_coords_decoder[0] = 'Beam'
+inst_coords_decoder[1] = 'Instrument'
+inst_coords_decoder[2] = 'Ship'
+inst_coords_decoder[3] = 'Earth'
 
 def main(files):
     filepath = files[1]
+    ts_start = None
+
+    times = []
+    head = []
+    pitch = []
+    roll = []
+    pressure = []
+    speed_of_sound = []
+
+    velocity = []
+
+    number_ensambles_read = 0
+
     with open(filepath, "rb") as binary_file:
         data = binary_file.read(2)
         while data:
-            print("hdr ", data)
+            # print("hdr ", data)
             if data == b'\x7f\x7f':
 
                 data = binary_file.read(2)
@@ -61,7 +87,7 @@ def main(files):
 
                 header = struct.unpack(header_decoder["unpack"], ensemble[0:2])
                 header_decoded = dict(zip(header_decoder['keys'], header))
-                print("header ", header_decoded)
+                #print("header ", header_decoded)
 
                 n = 2
                 addrs = [0 for x in range(0, header_decoded["dataTypes"])]
@@ -74,7 +100,7 @@ def main(files):
                 while n < (ensemble_len - 4):
                     data = ensemble[n:n+2]
                     n += 2
-                    print("data hdr ", data)
+                    #print("data hdr ", data)
                     if data == b'\x00\x00':
                         data = ensemble[n:n+57]
                         n += 57
@@ -88,14 +114,27 @@ def main(files):
                         variable = struct.unpack(variable_decoder["unpack"], data)
                         variable_decoded = dict(zip(variable_decoder['keys'], variable))
                         print("variable header ", variable_decoded)
-                        ts = datetime.datetime(year=variable_decoded['rtc_cen']*100 + variable_decoded['rtc_year'], month=variable_decoded['rtc_month'],
-                                               day=variable_decoded['rtc_day'],
-                                               hour=variable_decoded['rtc_hour'], minute=variable_decoded['rtc_min'], second=variable_decoded['rtc_sec'],
+                        ts = datetime.datetime(year=variable_decoded['rtc_cen']*100 + variable_decoded['rtc_year'],
+                                               month=variable_decoded['rtc_month'], day=variable_decoded['rtc_day'],
+                                               hour=variable_decoded['rtc_hour'], minute=variable_decoded['rtc_min'],
+                                               second=variable_decoded['rtc_sec'],
                                                microsecond=variable_decoded['rtc_hsec']*1000*10)
+
                         print("ts = ", ts)
+                        if not ts_start:
+                            ts_start = ts
+                        times.append(ts)
+
+                        head.append(variable_decoded['heading']*0.01)
+                        pitch.append(variable_decoded['pitch']*0.01)
+                        roll.append(variable_decoded['roll']*0.01)
+                        pressure.append(variable_decoded['pressure']/1000)
+
+                        number_ensambles_read += 1
 
                     if data == b'\x00\x01':  # velocity data
-                        data = ensemble[n:n+8*fixed_decoded['num_cells']]
+                        data = ensemble[n:n+(4*2)*fixed_decoded['num_cells']]
+                        velocity.append(struct.unpack("<%dh"%(4*fixed_decoded['num_cells']), data))
                         n += len(data)
                     elif data == b'\x00\x02':  # correlation mag
                         data = ensemble[n:n+4*fixed_decoded['num_cells']]
@@ -119,13 +158,74 @@ def main(files):
 
     ncOut = Dataset(outputName, 'w', format='NETCDF4')
 
+    inst_system = fixed_decoded['sysConfig'] & 0x7
+    inst_system_text = inst_system_decoder[inst_system]
+    print("system ", inst_system_text)
+
     # add global attributes
-    instrument_model = 'WORKHORSE'
+    instrument_model = 'WORKHORSE ' + inst_system_text
     instrument_serialnumber = fixed_decoded['inst_serial']
 
     ncOut.instrument = 'RDI - ' + instrument_model
     ncOut.instrument_model = instrument_model
-    ncOut.instrument_serial_number = instrument_serialnumber
+    ncOut.instrument_serial_number = str(instrument_serialnumber)
+    ncOut.data_coordinates = inst_coords_decoder[(fixed_decoded['coord_trans'] >> 3) & 0x03]
+
+    # add time variable
+
+    #     TIME:axis = "T";
+    #     TIME:calendar = "gregorian";
+    #     TIME:long_name = "time";
+    #     TIME:units = "days since 1950-01-01 00:00:00 UTC";
+
+    tDim = ncOut.createDimension("TIME", number_ensambles_read)
+    ncTimesOut = ncOut.createVariable("TIME", "d", ("TIME",), zlib=True)
+    ncTimesOut.long_name = "time"
+    ncTimesOut.units = "days since 1950-01-01 00:00:00 UTC"
+    ncTimesOut.calendar = "gregorian"
+    ncTimesOut.axis = "T"
+    ncTimesOut[:] = date2num(times, calendar=ncTimesOut.calendar, units=ncTimesOut.units)
+
+    cellDim = ncOut.createDimension("CELL", fixed_decoded["num_cells"])
+
+    var_head = ncOut.createVariable("HEADING_MAG", "f8", ("TIME",), zlib=True)
+    var_head[:] = head
+    var_head.units = 'degrees'
+    var_pitch = ncOut.createVariable("PITCH", "f8", ("TIME",), zlib=True)
+    var_pitch[:] = pitch
+    var_pitch.units = 'degrees'
+    var_roll = ncOut.createVariable("ROLL", "f8", ("TIME",), zlib=True)
+    var_roll[:] = roll
+    var_roll.units = 'degrees'
+    var_press = ncOut.createVariable("PRES", "f8", ("TIME",), zlib=True)
+    var_press[:] = pressure
+    var_press.units = 'dbar'
+
+    var_vel = ncOut.createVariable("UCUR", "f8", ("TIME", "CELL"), zlib=True)
+    v = np.array(velocity).reshape([number_ensambles_read, fixed_decoded['num_cells'], fixed_decoded['num_beam']])
+    var_vel[:] = v[:, :, 0]
+    var_vel.units = 'm/s'
+    var_vel = ncOut.createVariable("VCUR", "f8", ("TIME", "CELL"), zlib=True)
+    v = np.array(velocity).reshape([number_ensambles_read, fixed_decoded['num_cells'], fixed_decoded['num_beam']])
+    var_vel[:] = v[:, :, 1]
+    var_vel.units = 'm/s'
+    var_vel = ncOut.createVariable("WCUR", "f8", ("TIME", "CELL"), zlib=True)
+    v = np.array(velocity).reshape([number_ensambles_read, fixed_decoded['num_cells'], fixed_decoded['num_beam']])
+    var_vel[:] = v[:, :, 2]
+    var_vel.units = 'm/s'
+    var_vel = ncOut.createVariable("ECUR", "f8", ("TIME", "CELL"), zlib=True)
+    v = np.array(velocity).reshape([number_ensambles_read, fixed_decoded['num_cells'], fixed_decoded['num_beam']])
+    var_vel[:] = v[:, :, 3]
+    var_vel.units = 'm/s'
+
+    ncTimeFormat = "%Y-%m-%dT%H:%M:%SZ"
+
+    ncOut.setncattr("time_coverage_start", ts_start.strftime(ncTimeFormat))
+    ncOut.setncattr("time_coverage_end", ts.strftime(ncTimeFormat))
+
+    # add creating and history entry
+    ncOut.setncattr("date_created", datetime.datetime.utcnow().strftime(ncTimeFormat))
+    ncOut.setncattr("history", datetime.datetime.utcnow().strftime("%Y-%m-%d") + " created from file " + filepath)
 
     return outputName
 